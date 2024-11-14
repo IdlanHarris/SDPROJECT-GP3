ed8afbe38848ae0160f0b3f237394b2c720d5a4f
IdlanHarris <idlanutm22@gmail.com>
2024-11-15 00:48:13 +0800

    delete

--- php/Logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // Start the session
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-header("Location: /"); // Redirect to login page (change the URL to your login page)
-exit(); // Ensure no further code is executed
-?>
